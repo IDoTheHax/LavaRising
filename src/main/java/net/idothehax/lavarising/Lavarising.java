4a185803830ec3599f8c3d379b60807cfb0326e3
IDoTheHax <hax@idothehax.com>
2025-03-10 21:44:27 +0100

    Init Project

---
+++ src/main/java/net/idothehax/lavarising/Lavarising.java
@@ -0,0 +1,11 @@
+package net.idothehax.lavarising;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Lavarising implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
